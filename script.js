da77e3fe101464eec60a420423d410a641e5ad09
Shawn Stern <stern.shawn@gmail.com>
2016-09-04 09:55:24 -0700

    Updating the way I access the JSON to be cleaner, ie. dot notation for established fields and square brackets to access element by variables. Cleaning up old comments and console.log clutter from debugging stages.

--- script.js
+++ script.js
@@ -1,59 +1,37 @@
 $(document).ready(function() {
-
   $('#submitButton').click(function() {
-    // Grab the user input
-    var article = $('#inputArticle').val();
-    console.log("User is requesting a search for: " + article);
-
-    // var json = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
-    // var jsonp = "&callback=?";
+    // Grab the user input and create our API query URL
+    var article = $('#inputArticle').val()
     var json = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max&callback=?&gsrsearch="
-
-    // Append user input to base json query string and include callback at end to ensure compatibility
-    var query = json + article; // + jsonp;
-    console.log("Submitted for JSON: " + query);
+    var query = json + article
 
     $.getJSON(query, function(data) {
-      console.log("success");
-
-      console.log(JSON.stringify(data));
-
       // Create list of results, if we got any
       if (data.query) {
-        console.log('Results returned successfully');
+        $('#fillMe').html('<h1><em>Top Results</em></h1>')
 
-        // Clear out any pre-existing content from prior searches, etc
-        console.log("Clearing any previous states...");
-        $('#fillMe').html('<h1><em>Top Results</em></h1>');
-
-        // Iterate over each page that was returned, build up the html for that entry and append to the list of current results
-        for (var key in data["query"]["pages"]) {
-          console.log(data["query"]["pages"][key]);
-
+        // Iterate over each page that was returned, build up the html for that
+        // entry and append to the list of current results
+        for (var key in data.query.pages) {
+          // Have to use bracket notation to access fields using variables...
           // Hold result in short-named var here to reduce typing in the concatenations...
-          var wiki = data["query"]["pages"][key];
+          var wiki = data.query.pages[key]
           var newDiv = "<a href='http://en.wikipedia.org/?curid=" + wiki.pageid +
-            "' target='_blank'><div class='resultItem'><h2>" + wiki.title +
-            "</h2>\n<p>" + wiki.extract +
-            "</p></div></a>";
-
-          console.log(newDiv);
-          $('#fillMe').append(newDiv);
+              "' target='_blank'><div class='resultItem'><h2>" + wiki.title +
+              "</h2>\n<p>" + wiki.extract + "</p></div></a>"
+          $('#fillMe').append(newDiv)
         }
+      } else {
+        // Otherwise, print out an error to let the user know what happened
+        $('#fillMe').html('<h1><em>No results for that search...</em></h1>')
       }
-      // Otherwise, print out an error to let the user know what happened
-      else {
-        console.log('User search returned nothing...');
-        $('#fillMe').html('<h1><em>No results for that search...</em></h1>');
-      }
-    });
-  });
+    })
+  })
 
-  // If user presses enter while in the text box, treat the same as having clicked the submit button
+  // If user presses enter in the text box, treat as clicking the submit button
   $("#inputArticle").keyup(function(event) {
     if (event.keyCode == 13) {
-      $("#submitButton").click();
+      $("#submitButton").click()
     }
-  });
-
-});
+  })
+})
